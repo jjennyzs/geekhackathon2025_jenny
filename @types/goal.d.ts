6dc5e980a56c676872bc4770bd61c6c4ad9efd4d
Satoshi Tomita <S.Tomita2023@keio.jp>
2025-12-08 23:38:05 +0900

    Goalの型修正

--- @types/goal.d.ts
+++ @types/goal.d.ts
@@ -1,5 +1,6 @@
 export type Goal = {
-  title: string;
-  weight: number;
-  feedback: string;
+  title: string; //各目標のタイトル
+  weight: number; //各目標のウェイト（大変さ）
+  feedback: string; //目標達成時のフィードバック
+  isFinished:boolean; //目標が達成されたかどうか
 };
